0a25d9d94e1f3879250704a540309f0c57aae6f6
cbichis <cristi@imagis.ro>
2019-01-04 11:31:52 +0200

    Update Extension.php

--- library/Zend/Validate/File/Extension.php
+++ library/Zend/Validate/File/Extension.php
@@ -185,6 +185,10 @@ class Zend_Validate_File_Extension extends Zend_Validate_Abstract
      */
     public function isValid($value, $file = null)
     {
+        //fix (kind of hack actually) for empty
+    	if(empty($value)){
+    		return true;
+		}
         // Is file readable ?
         #require_once 'Zend/Loader.php';
         if (!Zend_Loader::isReadable($value)) {
